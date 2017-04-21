4fba965b21ad212ac7d45abe4617d10aef10820e
FerencOnodi <onodiferenc91@gmail.com>
2017-04-21 12:39:19 +0200

    Created: FilePartReader Test.txt

---
+++ src/FilePartReader.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by ferenc on 2017.04.21..
+ */
+public class FilePartReader {
+
+    String filePath;
+    Integer fromLine;
+    Integer toLine;
+
+    public FilePartReader(String filePath, Integer fromLine, Integer toLine) {
+        if(toLine < fromLine || fromLine < 1) {
+            throw new IllegalArgumentException();
+        }
+        this.filePath = filePath;
+        this.fromLine = fromLine;
+        this.toLine = toLine;
+    }
+
+    private String read() throws IOException {
+        FileReader fileReader = new FileReader(filePath);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        String content = "";
+        String currentLine = bufferedReader.readLine();
+
+        while (currentLine != "") {
+            content += currentLine;
+        }
+
+        return content;
+    }
+
+    public String readLines() throws IOException {
+        String content = this.read();
+        String result = "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String[] lines = content.split("\n");
+
+        for (int temp = 0; temp < content.length(); temp++) {
+            if (temp <= (fromLine - 1) && temp < toLine) {
+                stringBuilder.append(lines[temp] + "\n");
+            }
+        }
+
+        result = stringBuilder.toString();
+        return result;
+    }
+}
